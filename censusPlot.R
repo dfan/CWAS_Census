3055684b5eeb7ce18dbfe137a3cafc958ef071e9
dfan97 <davidfan219@gmail.com>
2016-06-25 00:42:57 -0400

    Added files

--- censusPlot.R
+++ censusPlot.R
@@ -12,7 +12,7 @@ names(df) <- c("region", "value")
 # remove leading zeros from FIP codes. Choropleth requires numeric column type in dataframe
 df$region <- as.numeric(sapply(df$region, function(y) sub('^0+([1-9])', '\\1', y)))
 
-setwd('/Users/dfan/Dropbox/Research\ Lab\ Projects/Undergraduate/Harvard-MIT\ 2016/Code/CWAS_PartTwo')
+setwd('/Users/dfan/Dropbox/Research\ Lab\ Projects/Undergraduate/Harvard-MIT\ 2016/Code/CWAS_Census')
 df$value <- as.numeric(as.character(data$medianincome))
 png('medianIncome.png', units="px", width=1600, height=1600, res=150)
 county_choropleth(df, title = "", legend = "", num_colors = 7, state_zoom = NULL, county_zoom = NULL, reference_map = FALSE) + ggtitle("Counties by Median Income")
