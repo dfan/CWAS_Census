8eda5c2113cda2348a997c00d2f2b6cad3e217ad
dfan97 <davidfan219@gmail.com>
2016-06-25 01:10:47 -0400

    Reduced image height

--- censusPlot.R
+++ censusPlot.R
@@ -1,5 +1,5 @@
 library(RMySQL)
-library(choroplethrMaps)
+library(choroplethr)
 library(ggplot2)
 
 con <- dbConnect(MySQL(), user = "root", password = "root", dbname = "census2010", unix.sock="/Applications/MAMP/tmp/mysql/mysql.sock")
@@ -12,23 +12,25 @@ names(df) <- c("region", "value")
 # remove leading zeros from FIP codes. Choropleth requires numeric column type in dataframe
 df$region <- as.numeric(sapply(df$region, function(y) sub('^0+([1-9])', '\\1', y)))
 
+# set to directory you want the images to appear in
 setwd('/Users/dfan/Dropbox/Research\ Lab\ Projects/Undergraduate/Harvard-MIT\ 2016/Code/CWAS_Census')
+
 df$value <- as.numeric(as.character(data$medianincome))
-png('medianIncome.png', units="px", width=1600, height=1600, res=150)
+png('medianIncome.png', units="px", width=1600, height=1000, res=150)
 county_choropleth(df, title = "", legend = "", num_colors = 7, state_zoom = NULL, county_zoom = NULL, reference_map = FALSE) + ggtitle("Counties by Median Income")
 dev.off()
 
 df$value <- as.numeric(as.character(data$population))
-png('population.png', units="px", width=1600, height=1600, res=150)
+png('population.png', units="px", width=1600, height=1000, res=150)
 county_choropleth(df, title = "", legend = "", num_colors = 7, state_zoom = NULL, county_zoom = NULL, reference_map = FALSE) + ggtitle("Counties by Total Population")
 dev.off()
 
 df$value <- as.numeric(as.character(data$black))
-png('blackFrac.png', units="px", width=1600, height=1600, res=150)
+png('blackFrac.png', units="px", width=1600, height=1000, res=150)
 county_choropleth(df, title = "", legend = "", num_colors = 7, state_zoom = NULL, county_zoom = NULL, reference_map = FALSE) + ggtitle("Counties by Black Fraction")
 dev.off()
 
 df$value <- as.numeric(as.character(data$white))
-png('whiteFrac.png', units="px", width=1600, height=1600, res=150)
+png('whiteFrac.png', units="px", width=1600, height=1000, res=150)
 county_choropleth(df, title = "", legend = "", num_colors = 7, state_zoom = NULL, county_zoom = NULL, reference_map = FALSE) + ggtitle("Counties by White Fraction")
 dev.off()
